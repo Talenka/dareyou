484c9e548485f680bf981b98a5176a7acd04aae2
Boudah Talenka <boudah.talenka@gmail.com>
2013-06-21 18:16:43 +0200

    ...

--- language.php
+++ language.php
@@ -6,8 +6,6 @@ require_once 'core.php';
 
 $l = preg_replace('/[^a-z]/', '', strtolower($_SERVER['QUERY_STRING']));
 
-if (file_exists('lang.' . $l . '.php')) {
-    setcookie('lang', $l, NOW + 31536000);
-}
+if (file_exists('lang.' . $l . '.php')) setcookie('lang', $l, NOW + 31536000);
 
 redirectTo('.');
