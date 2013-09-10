3289a333005436269f2528f68112e29e65652dcd
Boudah Talenka <boudah.talenka@gmail.com>
2013-09-10 16:23:33 +0200

    Fix typo

--- config.sample.php
+++ config.sample.php
@@ -12,25 +12,25 @@ namespace Dareyou;
 // error_reporting(E_ALL);
 
 // The <title> of the site
-const 'SITE_TITLE' = 'DareYou!';
+const SITE_TITLE = 'DareYou!';
 
 // The password hashing salt (please choose a long and unique one)
-const 'CRYPT_SALT' = 'KM4WcTe+tiPzmkc{';
+const CRYPT_SALT = 'KM4WcTe+tiPzmkc{';
 
 // The MySql database host ("localhost" in 99% of cases)
-const 'SQL_HOST' = 'localhost';
+const SQL_HOST = 'localhost';
 
 // The MySql database username
-const 'SQL_USER' = 'myhumblesite';
+const SQL_USER = 'myhumblesite';
 
 // The MySql database password (please choose a long and unique one)
-const 'SQL_PASSWORD' = 'b62kH043b6wt4T6Q';
+const SQL_PASSWORD = 'b62kH043b6wt4T6Q';
 
 // Your MySql database name
-const 'SQL_DB' = 'humblesite';
+const SQL_DB = 'humblesite';
 
 // The MD5 digest of admin email (yours maybe)
-const 'ADMIN_HASH' = '00000000000000000000000000000000';
+const ADMIN_HASH = '00000000000000000000000000000000';
 
 // The canonical domain name
-const 'SERVER_NAME' = 'dareyou2.be';
+const SERVER_NAME = 'dareyou2.be';
